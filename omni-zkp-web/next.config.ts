5cb1a7cec3c051aea368ff3a3db7c929f82fdd5e
Krishanu Roy <krishanuroygit@gmail.com>
2026-03-01 19:21:31 +0530

    adds local

--- omni-zkp-web/next.config.ts
+++ omni-zkp-web/next.config.ts
@@ -4,12 +4,10 @@ const nextConfig: NextConfig = {
   // Hide the Next.js bottom-left dev indicator
   devIndicators: false,
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  webpack: (config: any) => {
-    // Enable WebAssembly in Webpack
+  webpack(config) {
     config.experiments = {
+      ...config.experiments,
       asyncWebAssembly: true,
-      layers: true,
     };
     return config;
   },
